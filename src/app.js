247b9c5a41c71d5cb8e3cedf70110032467b00e1
nicoleliano <56807477+nicoleliano@users.noreply.github.com>
2021-06-01 20:25:44 -0400

    Updated the time/date

--- src/app.js
+++ src/app.js
@@ -1,12 +1,31 @@
+function formateDate(timestamp) {
+  let date = new Date(timestamp);
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`
+  };
+  if (hours > 12) {
+    hours = hours - 12;
+  };
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  };
+  return `${day} ${hours}:${minutes}`;
+};
+
 function displayTemperature(response) {
-  console.log(response);
   let cityElement = document.querySelector('#city');
+  let dateElement = document.querySelector('#date');
   let descriptionElement = document.querySelector('#description');
   let humidityElement = document.querySelector('#humidity');
   let temperatureElement = document.querySelector('#temperature');
   let windElement = document.querySelector('#wind');
   
   cityElement.innerHTML = response.data.name;
+  dateElement.innerHTML = formateDate(response.data.dt * 1000);
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   temperatureElement.innerHTML = Math.round(response.data.main.temp);
